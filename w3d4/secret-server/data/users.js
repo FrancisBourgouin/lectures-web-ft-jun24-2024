b5988ae4bf1cc3a49ad8045284f343bf0fa4cfbd
Francis Bourgouin <netbeuix@gmail.com>
2024-07-11 14:38:28 -0400

    add w3d4 lecture

--- w3d4/secret-server/data/users.js
+++ w3d4/secret-server/data/users.js
@@ -1,13 +1,11 @@
 const user1 = {
   name: "Dimitri Ivanovich Mendeleiv",
-  password: "hydrogen",
   email: "periodic@table.com",
   secret: "He actually prefers biology over chemistry",
 };
 
 const user2 = {
   name: "Doug Judy",
-  password: "rosa",
   email: "pontiac@bandit.com",
   secret: "He doesn't know how to drive stick",
 };
